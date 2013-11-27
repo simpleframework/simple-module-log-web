f79ddf9acd8b9974bae3022429c32ab8279fdbb4
simpleframework <cknet@126.com>
2013-11-27 13:26:30 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/log/web/LogWebContext.java
+++ src/main/java/net/simpleframework/module/log/web/LogWebContext.java
@@ -1,9 +1,11 @@
 package net.simpleframework.module.log.web;
 
 import static net.simpleframework.common.I18n.$m;
+import net.simpleframework.ctx.IApplicationContext;
 import net.simpleframework.ctx.Module;
 import net.simpleframework.module.log.impl.LogContext;
 import net.simpleframework.module.log.web.page.t1.EntityDeleteLogMgrPage;
+import net.simpleframework.mvc.IMVCContextVar;
 import net.simpleframework.mvc.ctx.WebModuleFunction;
 
 /**
@@ -12,7 +14,24 @@ import net.simpleframework.mvc.ctx.WebModuleFunction;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class LogWebContext extends LogContext {
+public class LogWebContext extends LogContext implements IMVCContextVar {
+
+	@Override
+	public void onInit(final IApplicationContext application) throws Exception {
+		super.onInit(application);
+
+		ctx.addFilterListener(sListener = new PVStatFilterListener());
+	}
+
+	private PVStatFilterListener sListener;
+
+	@Override
+	public void onShutdown(final IApplicationContext application) throws Exception {
+		super.onShutdown(application);
+		if (sListener != null) {
+			sListener.updateStats();
+		}
+	}
 
 	@Override
 	protected Module createModule() {
---
+++ src/main/java/net/simpleframework/module/log/web/PVStatFilterListener.java
@@ -0,0 +1,77 @@
+package net.simpleframework.module.log.web;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.servlet.FilterChain;
+
+import net.simpleframework.common.Convert;
+import net.simpleframework.common.StringUtils;
+import net.simpleframework.module.log.ILogContextAware;
+import net.simpleframework.module.log.IPVLogService;
+import net.simpleframework.module.log.PVLog;
+import net.simpleframework.mvc.IFilterListener;
+import net.simpleframework.mvc.PageRequestResponse;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
+public class PVStatFilterListener implements IFilterListener, ILogContextAware {
+
+	class PVStat {
+		int pv = 0;
+
+		Set<String> uv = new HashSet<String>();
+
+		Set<String> ip = new HashSet<String>();
+	}
+
+	final Map<String, PVStat> stats = new ConcurrentHashMap<String, PVStat>();
+
+	void updateStats() {
+		final IPVLogService lservice = context.getPVLogService();
+		for (final Map.Entry<String, PVStat> e : stats.entrySet()) {
+			final String[] arr = StringUtils.split(e.getKey(), "-");
+			final PVLog log = lservice.getPVLog(Convert.toInt(arr[0]), Convert.toInt(arr[1]),
+					Convert.toInt(arr[2]), Convert.toInt(arr[3]));
+			final PVStat _stat = e.getValue();
+			log.setPv(log.getPv() + _stat.pv);
+			log.setUv(log.getUv() + _stat.uv.size());
+			log.setIp(log.getIp() + _stat.ip.size());
+			lservice.update(new String[] { "pv", "uv", "ip" }, log);
+		}
+		stats.clear();
+	}
+
+	@Override
+	public EFilterResult doFilter(final PageRequestResponse rRequest, final FilterChain filterChain)
+			throws IOException {
+		if (rRequest.isHttpRequest()) {
+			final String dk = Convert.toDateString(new Date(), "yyyy-MM-dd-HH");
+			PVStat stat = stats.get(dk);
+			if (stat == null) {
+				updateStats();
+				stats.put(dk, stat = new PVStat());
+			}
+			stat.pv++;
+			stat.ip.add(rRequest.getRemoteAddr());
+			Object userId;
+			final String sessionId = rRequest.getSessionId();
+			if ((userId = rRequest.getLoginId()) != null) {
+				if (!stat.uv.contains(sessionId)) {
+					stat.uv.add(Convert.toString(userId));
+				}
+			} else {
+				stat.uv.add(sessionId);
+			}
+		}
+		return EFilterResult.SUCCESS;
+	}
+}
