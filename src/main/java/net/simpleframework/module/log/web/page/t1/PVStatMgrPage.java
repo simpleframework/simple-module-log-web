61df61c96f9f6b39d0f5edcfd6e67ce359cb61dc
simpleframework <cknet@126.com>
2013-11-28 18:44:05 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/log/web/page/t1/PVStatMgrPage.java
+++ src/main/java/net/simpleframework/module/log/web/page/t1/PVStatMgrPage.java
@@ -13,6 +13,11 @@ import net.simpleframework.module.log.ILogContextAware;
 import net.simpleframework.module.log.PVLog;
 import net.simpleframework.mvc.PageMapping;
 import net.simpleframework.mvc.PageParameter;
+import net.simpleframework.mvc.common.element.CalendarInput;
+import net.simpleframework.mvc.common.element.ElementList;
+import net.simpleframework.mvc.common.element.InputElement;
+import net.simpleframework.mvc.common.element.Option;
+import net.simpleframework.mvc.common.element.SpanElement;
 import net.simpleframework.mvc.common.element.TabButtons;
 import net.simpleframework.mvc.component.ext.highchart.DataObj;
 import net.simpleframework.mvc.component.ext.highchart.EChart.EAxisType;
@@ -24,6 +29,7 @@ import net.simpleframework.mvc.component.ext.highchart.HcTooltip;
 import net.simpleframework.mvc.component.ext.highchart.HcXAxis;
 import net.simpleframework.mvc.component.ext.highchart.HcYAxis;
 import net.simpleframework.mvc.component.ext.highchart.HighchartBean;
+import net.simpleframework.mvc.component.ui.calendar.CalendarBean;
 import net.simpleframework.mvc.template.t1.T1ResizedTemplatePage;
 
 /**
@@ -39,10 +45,13 @@ public class PVStatMgrPage extends T1ResizedTemplatePage implements ILogContextA
 	protected void onForward(final PageParameter pp) {
 		super.onForward(pp);
 
+		addComponentBean(pp, "PVStatMgrPage_cal", CalendarBean.class).setCloseCallback(
+				"$Actions.loc('" + url(PVStatMgrPage.class) + "?date=' + date.format('yyyy-MM-dd'))");
+
 		final HighchartBean hc = (HighchartBean) addComponentBean(pp, "PVStatMgrPage_lchart",
 				HighchartBean.class).setContainerId("idPVStatMgrPage_lchart");
 
-		hc.setChart(new HcChart().setType(EHcType.line).setMarginTop(40)).setTitle("");
+		hc.setChart(new HcChart().setHeight(450).setType(EHcType.line).setMarginTop(40)).setTitle("");
 		hc.setTooltip(new HcTooltip().setUseHTML(true).setHeaderFormat("{point.key}H<br/>"));
 
 		final List<String> l = new ArrayList<String>();
@@ -55,7 +64,7 @@ public class PVStatMgrPage extends T1ResizedTemplatePage implements ILogContextA
 
 		hc.setyAxis(new HcYAxis().setTitle($m("PVStatMgrPage.2")));
 
-		final String[] arr = StringUtils.split(Convert.toDateString(new Date(), "yyyy-MM-dd"), "-");
+		final String[] arr = StringUtils.split(Convert.toDateString(getDate(pp), "yyyy-MM-dd"), "-");
 		final Map<Integer, PVLog> data = context.getPVLogService().getHourStat(Convert.toInt(arr[0]),
 				Convert.toInt(arr[1]), Convert.toInt(arr[2]));
 
@@ -79,6 +88,24 @@ public class PVStatMgrPage extends T1ResizedTemplatePage implements ILogContextA
 			h.addData(new DataObj(i < 10 ? "0" + i : String.valueOf(i), log == null ? 0 : log.getIp()));
 		}
 		hc.addSeries(h);
+	}
+
+	private Date getDate(final PageParameter pp) {
+		final Date d = Convert.toDate(pp.getParameter("date"), "yyyy-MM-dd");
+		return d == null ? new Date() : d;
+	}
+
+	@Override
+	public ElementList getLeftElements(final PageParameter pp) {
+		return ElementList.of(
+				new CalendarInput("idPVStatMgrPage_cal").setCalendarComponent("PVStatMgrPage_cal")
+						.setText(Convert.toDateString(getDate(pp), "yyyy-MM-dd")), SpanElement.SPACE15);
+	}
+
+	@Override
+	public ElementList getRightElements(final PageParameter pp) {
+		return ElementList.of(InputElement.select().addElements(new Option("按小时"), new Option("按天"),
+				new Option("按月")));
 	}
 
 	@Override
