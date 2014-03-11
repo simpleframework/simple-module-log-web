ec1b3731dba7ac8f3a462d77d21faa131916c400
simpleframework <cknet@126.com>
2014-03-11 16:05:59 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/log/web/page/t1/AbstractLogMgrPage.java
+++ src/main/java/net/simpleframework/module/log/web/page/t1/AbstractLogMgrPage.java
@@ -61,9 +61,9 @@ public abstract class AbstractLogMgrPage extends T1ResizedTemplatePage implement
 
 	protected TablePagerBean addTablePagerBean(final PageParameter pp,
 			final Class<? extends LogTbl> tblClass) {
-		return (TablePagerBean) addTablePagerBean(pp, "AbstractLogMgrPage_tbl").setShowLineNo(true)
-				.setPageItems(50).setPagerBarLayout(EPagerBarLayout.bottom)
-				.setContainerId("tbl_" + hashId).setHandlerClass(tblClass);
+		return (TablePagerBean) addTablePagerBean(pp, "AbstractLogMgrPage_tbl").setPageItems(50)
+				.setPagerBarLayout(EPagerBarLayout.bottom).setContainerId("tbl_" + hashId)
+				.setHandlerClass(tblClass);
 	}
 
 	protected abstract IDbBeanService<?> getBeanService();
