75dc953820f1dfb16dc9a11ca5f3634d7416828b
CHENK <CHENK@CHENK-PC>
2016-04-07 18:02:38 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/log/web/hdl/AbstractAttachmentLogHandler.java
+++ src/main/java/net/simpleframework/module/log/web/hdl/AbstractAttachmentLogHandler.java
@@ -25,7 +25,8 @@ import net.simpleframework.mvc.component.ext.attachments.AttachmentUtils;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public abstract class AbstractAttachmentLogHandler<T extends Attachment, M extends AbstractIdBean>
